32213674e06b32474e0b5cd270e258b6a964e4b0
charan k <charancr1630@gmail.com>
2023-10-09 17:14:40 +0530

    introduction update02

--- Introduction.go
+++ Introduction.go
@@ -6,17 +6,23 @@ Go is a new gen programming language designed for modren era
 
 it is like a tool box for creating the cool machines in the digital world
 let we just open up the tool box and familiarize with the tools and packages(bundled up tool for specific use)
-we need to fix up the screws and the pipes
-
+we need to fix up the screws and the pipes inside the machine to get the desired output
 
 
 let we know some terms
 module- packages are grouped into module
 package - code is grouped into package
 
-
-
-
 Let dive with this simple example
 
+steps to follow
+
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello Universe")
+}
