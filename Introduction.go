143f1d80092bf340000b45b27b16ffb92a819cdc
charan k <charancr1630@gmail.com>
2023-10-09 13:29:30 +0530

    introductio update01

--- Introduction.go
+++ Introduction.go
@@ -1,9 +1,22 @@
 /*
-Go is 
- is a tool box for creating the cool machines in the digital world
+Go is a new gen programming language designed for modren era
+--compiled
+-- minimalistic approach
+--contains libraries which literally gives us the full controll from os
+
+it is like a tool box for creating the cool machines in the digital world
+let we just open up the tool box and familiarize with the tools and packages(bundled up tool for specific use)
+we need to fix up the screws and the pipes
+
+
+
+let we know some terms
+module- packages are grouped into module
+package - code is grouped into package
 
 
 
 
+Let dive with this simple example
 
 */
